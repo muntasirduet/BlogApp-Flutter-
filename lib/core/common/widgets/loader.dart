cdd201d468a51710357bb2802e476b0cdae92660
muntasir <mmmduetcse@gmail.com>
2024-03-15 13:35:45 +0600

    Modified Loder Class

--- lib/core/common/widgets/loader.dart
+++ lib/core/common/widgets/loader.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
   const Loader({super.key});
-
+//this is loader for this app
   @override
   Widget build(BuildContext context) {
     return const Center(
